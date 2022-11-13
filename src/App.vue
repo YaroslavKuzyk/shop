49dc4f8b20581b4532f080d84efdac6ee3fa43f0
Yaroslav Kuzyk <y.kuzyk@wpt.dev>
2022-11-13 02:00:50 +0200

    refactoring admin page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   v-app
     v-main
-      MainHeader
+      MainHeader(v-if="!isAdminPage")
       router-view
-      MainFooter
+      MainFooter(v-if="!isAdminPage")
 </template>
 
 <script>
@@ -15,6 +15,11 @@ export default {
   components: {MainFooter, MainHeader},
   data: () => ({
   }),
+  computed: {
+    isAdminPage() {
+      return this.$route.path.slice(0, 6) === '/admin' ? true : false
+    }
+  }
 };
 </script>
 <style lang="scss" src="./assets/styles/main.scss"></style>
